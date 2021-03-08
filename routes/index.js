fbea2a8099e2d5369c27d3bda73499615bec63a0
Magnus Stigson <stigson87@gmail.com>
2021-03-08 13:54:46 +0100

    Update to make app installable

--- routes/index.js
+++ routes/index.js
@@ -4,15 +4,24 @@ const fs = require('fs');
 const readline = require('readline');
 
 var renderRoot = function (req, res, next) {
-  fs.readFile('../shinemonitor_api/energy_now.txt', 'utf8', function (err,data) {
+  fs.readFile('/Users/Mac/energy_now.txt', 'utf8', function (err,data) {
     if (err) {
       return console.log(err);
     }
     console.log(data);
-    splitString = data.split('\n');
+    splitString = data.split('\n')
+    lastUpdate = splitString[0]
     powerNow = Number(splitString[1])
+    unit = "W"
+    if (powerNow > 1000){
+      //Round with one decimal
+      powerNow = powerNow/100
+      powerNow = Math.round(powerNow)
+      powerNow = powerNow/10
+      unit = "kW"
+    }
 
-    res.render('body', {powerNow: powerNow/1000});
+    res.render('body', {powerNow: powerNow, unit:  unit, lastUpdate: lastUpdate});
   });
 }
 
--- start.js
+++ start.js
@@ -10,6 +10,10 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 app.use('/', routes);
 app.use(express.static('public'));
+app.use('/icons', express.static('icons'));
+app.use('/launch-screens', express.static('launch-screens'));
+app.use('/favicons', express.static('favicons'));
+app.use(express.static('pwa'));
 
 const server = http.listen(8181, () => {
   console.log(`Express is running on port ${server.address().port}`);
